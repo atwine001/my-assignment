3710a147de23a5c88fb9fd212e8f83400edb9c89
atwine001 <molinatwine@gmail.com>
2024-10-04 21:01:08 +0300

    atwine mooren 1

---
+++ src/loopsystem/Loopsystem.java
@@ -0,0 +1,41 @@
+package loopsystem;
+
+public class Loopsystem { 
+
+			public static void main(String[] args) {
+				//For Loop
+				System.out.println("For Loop");
+				for(int i=0; i<5; i++) {
+					System.out.println(i);
+				}
+				
+				//While Loop
+				System.out.println("While Loop");
+				int m=2;
+				while(m<=3) {System.out.println(m);
+				m++;
+				
+				}
+				//Do While Loop
+				System.out.println("Do While Loop");
+			int n=2;
+			do {
+				System.out.println(n);
+				n++;
+			}while(n<=3);
+			
+			
+			
+			
+			
+				
+				
+						
+				
+				
+					
+				
+				
+			}
+
+}
